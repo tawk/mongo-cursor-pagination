e15e6198fb26ba5e4fb3f44f592761f3ebc20097
Martins Lapselis <martins@tawk.to>
2023-04-10 17:06:41 +0800

    BREAKING: Remove mongoose and express related functionality

--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
-const config = require('./config');
 const aggregate = require('./aggregate');
+const config = require('./config');
 const find = require('./find');
-const findWithReq = require('./findWithReq');
 const search = require('./search');
-const sanitizeQuery = require('./utils/sanitizeQuery');
 const { encodePaginationTokens } = require('./utils/query');
-const mongoosePlugin = require('./mongoose.plugin');
+const sanitizeQuery = require('./utils/sanitizeQuery');
 
 module.exports = {
   config,
   find,
-  findWithReq,
   aggregate,
   search,
-  mongoosePlugin,
   sanitizeQuery,
   encodePaginationTokens,
 };
